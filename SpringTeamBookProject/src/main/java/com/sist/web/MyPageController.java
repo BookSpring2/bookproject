de843d8e1246917e5e60ba6424314f9e81a86b72
juhyun <juhyun@DESKTOP-OMBHQPM>
2021-10-26 17:29:40 +0900

    마이페이지

---
+++ SpringTeamBookProject/src/main/java/com/sist/web/MyPageController.java
@@ -0,0 +1,16 @@
+package com.sist.web;
+import com.sist.dao.*;
+import com.sist.vo.*;
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class MyPageController {
+	@Autowired
+	private MyPageDAO dao;
+	
+	
+}
