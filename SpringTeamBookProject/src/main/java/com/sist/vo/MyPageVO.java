37fef439d67d8aebdffcda881e1a803a1f738149
juhyun <juhyun@DESKTOP-OMBHQPM>
2021-10-27 16:53:06 +0900

    마이페이지

--- SpringTeamBookProject/src/main/java/com/sist/vo/MyPageVO.java
+++ SpringTeamBookProject/src/main/java/com/sist/vo/MyPageVO.java
@@ -1,5 +1,66 @@
 package com.sist.vo;
-
+/*
+ *  CARTID NOT NULL NUMBER        
+	USERID NOT NULL VARCHAR2(100) 
+	BOOKID NOT NULL NUMBER        
+	AMOUNT          NUMBER  
+ */
 public class MyPageVO {
-
+	private int cartid;
+	private String userid;
+	private String name;
+	private int bookid;
+	private String title;
+	private int price;
+	private int money;
+	private int amount;
+	public int getCartid() {
+		return cartid;
+	}
+	public void setCartid(int cartid) {
+		this.cartid = cartid;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getBookid() {
+		return bookid;
+	}
+	public void setBookid(int bookid) {
+		this.bookid = bookid;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getMoney() {
+		return money;
+	}
+	public void setMoney(int money) {
+		this.money = money;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	
 }
